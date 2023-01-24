994a8d612ece26ad8da70349e743db5ee63f60a9
Milena <71612657+milenahas@users.noreply.github.com>
2023-01-24 15:52:14 -0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule) },
-  { path: '', pathMatch: 'full', redirectTo: '' },
-  { path: '**', redirectTo: '' }
+  { path: 'home', loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule) },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
